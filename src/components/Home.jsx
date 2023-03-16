b65444f2c8be9ca1ddc551647b7b44e15f2de340
Omar Sharif <mohandesomar@gmail.com>
2023-03-16 19:40:56 +0200

    home commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,19 @@ const Home = () => {
   return (
     <>
       <section className="grid h-screen w-screen place-items-center bg-[url('/src/assets/3.jpg')] bg-cover bg-no-repeat pb-14 md:bg-[url('/src/assets/5.jpg')] md:pb-0 md:pl-36">
-        <div className="max-w-xs rounded-xl bg-black/50 p-4 text-white backdrop-blur-2xl">
-          <img className="w-40" src="/src/assets/8.png" alt="home image" />
-          <p className="text-xs">OMAR SHARIF ZAKI</p>
-          <p className="text-xl">FULL STACK WEB DEVELOPER</p>
-          <p className="mt-4">
+        <div className="max-w-xs rounded-xl bg-black/50 p-4 text-white backdrop-blur-2xl md:max-w-md md:p-8 lg:max-w-xl lg:p-10">
+          <img
+            className="mx-auto mb-8 w-44 md:w-48 lg:w-56"
+            src="/src/assets/8.png"
+            alt="home image"
+          />
+          <p className="text-xs lg:text-sm">OMAR SHARIF ZAKI</p>
+          <p className="text-xl lg:text-3xl">FULL STACK WEB DEVELOPER</p>
+          <p className="mt-4 lg:text-lg">
             I can build you whatever you want with alot of experience any thing
             sall be easy
           </p>
-          <button className="my-4 rounded-md bg-gray-900 px-6 py-4 transition active:scale-95 active:bg-gray-800 md:hover:bg-gray-800">
+          <button className="mt-4 rounded-md bg-gray-900 px-6 py-4 transition active:scale-95 active:bg-gray-800 md:hover:bg-gray-800 lg:text-xl">
             See Projects
           </button>
         </div>
