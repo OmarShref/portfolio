383c207f38544b975211251911f6765d4812b708
Omar Sharif <mohandesomar@gmail.com>
2023-03-17 20:26:13 +0200

    logo added nav style changed

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Nav = () => {
   return (
     <>
-      <section className="fixed bottom-0 flex w-full justify-around bg-black/80 text-white backdrop-blur-xl sm:text-lg  md:left-0 md:h-full md:w-auto md:flex-col md:justify-center">
+      <div className="fixed top-3 left-4 bg-black/80 p-1 text-white ring-1 ring-white ">
+        OS
+      </div>
+      <section className="fixed bottom-0 flex w-full justify-around rounded-tr-2xl rounded-tl-2xl bg-black/80 text-white backdrop-blur-xl sm:text-lg md:left-0  md:bottom-1/2 md:h-fit md:w-auto md:translate-y-1/2 md:flex-col md:justify-center md:rounded-tl-none md:rounded-br-2xl md:py-4">
         <a
           className="relative py-4 transition md:px-6 md:hover:bg-white/10"
           href="#home"
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Projects = () => {
   return (
     <>
-      <section id="projects" className="h-screen w-screen"></section>
+      <section
+        id="projects"
+        className="grid h-screen w-screen place-items-center bg-[url('/src/assets/2.jpg')] bg-cover bg-no-repeat pb-14 md:bg-[url('/src/assets/4.jpg')] md:pb-0 md:pl-36"
+      ></section>
     </>
   );
 };
