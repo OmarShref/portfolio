63b8bc2f782d928c16d7c9b9bbeb06a7b9fcfc8a
Omar Sharif <mohandesomar@gmail.com>
2023-03-17 19:53:31 +0200

    ui fixes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         id="home"
         className="grid h-screen w-screen place-items-center bg-[url('/src/assets/3.jpg')] bg-cover bg-no-repeat pb-14 md:bg-[url('/src/assets/5.jpg')] md:pb-0 md:pl-36"
       >
-        <div className="max-w-xs rounded-xl bg-black/50 p-4 text-white backdrop-blur-2xl md:max-w-md md:p-8 lg:max-w-xl lg:p-10">
+        <div className="max-w-xs rounded-xl bg-black/80 p-4 text-white backdrop-blur-2xl md:max-w-md md:p-8 lg:max-w-xl lg:p-10">
           <img
             className="mx-auto mb-8 w-44 md:w-48 lg:w-56"
             src={landingPhoto}
@@ -20,9 +20,11 @@ const Home = () => {
             I can build you whatever you want with alot of experience any thing
             sall be easy
           </p>
-          <button className="mt-4 rounded-md bg-gray-900 px-6 py-4 transition active:scale-95 active:bg-gray-800 md:hover:bg-gray-800 lg:text-xl">
-            See Projects
-          </button>
+          <a href="#projects">
+            <button className="mt-4 w-full rounded-md bg-white py-4 font-semibold text-black transition active:scale-95 active:bg-black active:text-white active:ring-1 active:ring-white md:hover:bg-black md:hover:text-white md:hover:ring-1 md:hover:ring-white lg:text-xl">
+              See Projects
+            </button>
+          </a>{" "}
         </div>
       </section>
     </>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Nav = () => {
   return (
     <>
-      <section className="fixed bottom-0 flex w-full justify-around bg-black/50 text-white backdrop-blur-xl sm:text-lg  md:left-0 md:h-full md:w-auto md:flex-col md:justify-center">
+      <section className="fixed bottom-0 flex w-full justify-around bg-black/80 text-white backdrop-blur-xl sm:text-lg  md:left-0 md:h-full md:w-auto md:flex-col md:justify-center">
         <a
           className="relative py-4 transition md:px-6 md:hover:bg-white/10"
           href="#home"
