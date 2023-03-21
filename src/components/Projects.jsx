60c320db9b31250e5167938dd685b756667e84b8
Omar Sharif <mohandesomar@gmail.com>
2023-03-21 15:57:46 +0200

    projects initialize

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,7 +6,9 @@ const Projects = () => {
       <section
         id="projects"
         className="grid h-screen w-screen place-items-center bg-[url('/src/assets/2.jpg')] bg-cover bg-no-repeat pb-14 md:bg-[url('/src/assets/4.jpg')] md:pb-0 md:pl-36"
-      ></section>
+      >
+        <div className="max-w-xs rounded-xl bg-black/80 p-4 text-white backdrop-blur-2xl md:max-w-md md:p-8 lg:max-w-xl lg:p-10"></div>
+      </section>
     </>
   );
 };
